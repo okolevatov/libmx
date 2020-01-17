3af3a3062b7a2f99bc85520c83632f1c27c940bb
okolevatov <53995584+okolevatov@users.noreply.github.com>
2020-01-17 13:00:59 +0200

    libmx
    Final version

---
+++ libmx3/src/mx_strnew.c
@@ -0,0 +1,17 @@
+#include "../inc/libmx.h"
+
+char *mx_strnew(const int size) {
+	if(size <= 0) {
+		return NULL;
+	}
+	char *buffer = (char*) malloc(size + 1);
+	if(buffer == NULL) {
+		return NULL;
+	}
+	for(int i = 0;i < size; i++) {
+		buffer[i] = '\0';
+	}
+	buffer[size] = '\0';
+
+	return buffer;
+}
